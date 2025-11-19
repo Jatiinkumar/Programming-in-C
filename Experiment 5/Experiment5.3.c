5a8398d1c35810a49b771f1c2844ec579a302af2
Jatiinkumar <jatiinkumar.official@gmail.com>
2025-11-19 22:54:31 +0530

    Update Experiment5.3.c

---
+++ Experiment 5/Experiment5.3.c
@@ -0,0 +1,25 @@
+/*Experiment 5: Variable and Scope of Variable:
+5.3- Declare variables within different code blocks (enclosed by curley braces)
+and test their accessibility within and outside those blocks*/
+
+#include <stdio.h>
+int main()
+{
+    printf("Name - Jatin Kumar\nSAP ID - 590027886\nCourse - BCA\nBatch - B6");
+    printf("\n-------------------------------------\n");
+
+    int x = 10;
+    printf("Outside block: x = %d\n", x);
+
+    {
+        int y = 20;
+        printf("Inside first block: x = %d, y = %d\n", x, y);
+    }
+
+    {
+        int z = 30;
+        printf("Inside second block: x = %d, z = %d\n", x, z);
+    }
+
+    return 0;
+}
